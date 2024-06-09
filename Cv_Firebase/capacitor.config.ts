603c8593a5fb6a6d4bc459708414a64b6dc3eb3d
DannyYanacallo1755 <danny9yanacallo@gmail.com>
2024-06-09 17:45:49 -0500

    Tu mensaje de commit

--- Cv_Firebase/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.cv.app',
-  appName: 'cvapp',
-  webDir: 'www'
-};
-
-export default config;
