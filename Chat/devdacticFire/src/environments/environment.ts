af64c6f55b8812adecb113521db7344af89e6f45
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2024-06-09 21:51:16 -0500

    Agregado el proyecto de Chat en tiempo real

---
+++ Chat/devdacticFire/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDe6ubkESSyfsbr5IZ0ejsLo5jQMbSoZhQ",
+    authDomain: "firechat-273ec.firebaseapp.com",
+    projectId: "firechat-273ec",
+    storageBucket: "firechat-273ec.appspot.com",
+    messagingSenderId: "91213015680",
+    appId: "1:91213015680:web:b095733fabf29c4e495465",
+    measurementId: "G-P8MGHHWM6X"
+  }
+};
